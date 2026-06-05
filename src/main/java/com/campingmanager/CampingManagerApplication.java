c69a78449ae3cad67c2fe80a4e633f21ea061865
Michael <skyfallez4316@gmail.com>
2026-06-05 11:39:48 +0200

    Inizializzazione progetto Spring Boot gestionale campeggio

---
+++ src/main/java/com/campingmanager/CampingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.campingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampingManagerApplication.class, args);
+	}
+
+}
